6d9fe0e2ce877a8147f9ca37a9e9dd37c75c472d
Amish Tufail <amish.tufail2002@gmail.com>
2023-07-13 01:07:45 +0300

    Initial Commit

---
+++ BeReal Clone/BeReal_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeReal_CloneApp.swift
+//  BeReal Clone
+//
+//  Created by Amish on 13/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeReal_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
